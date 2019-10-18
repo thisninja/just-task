7d4beb66f4ceb4d7380e8a3b8b5cd3af6eb164b1
Dmitry Shevchenko <myzenfeed@gmail.com>
2019-10-19 02:02:52 +0300

    add /tasks post route handler for creating a task

---
+++ server/src/routes/tasks.route.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const { auth } = require('../middleware/auth');
+const { Task } = require('../models/task.model');
+
+router.post('/', auth, (req, res) => {
+  const task = new Task({
+    text: req.body.text,
+    _author: req.user._id,
+    dueDate: new Date(req.body.dueDate).getTime(),
+  });
+
+  task.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+module.exports = router;
