4060d3595562f465287c31231d1e18052a1d0f1e
Dmitry Shevchenko <myzenfeed@gmail.com>
2019-10-18 23:22:39 +0300

    move db file into separate folder

---
+++ server/db/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const MONGO_URI = process.env.MONGODB_URI;
+
+mongoose.Promise = global.Promise;
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  keepAlive: true,
+  reconnectTries: Number.MAX_VALUE,
+});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error'));
+db.once('open', console.log.bind(console, 'Connection Succeeded'));
+
+module.exports = { mongoose };
