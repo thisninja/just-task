8688f8dac3c0f7bcc6ed6f6cb06835da3f0dd427
Dmitry Shevchenko <myzenfeed@gmail.com>
2019-10-19 01:40:49 +0300

    add /users/login post route handler

--- server/src/routes/users.route.js
+++ server/src/routes/users.route.js
@@ -19,4 +19,18 @@ router.post('/', (req, res) => {
   })
 });
 
+router.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  const body = { email, password };
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-access-token', token).header('expiresIn', 3600).send(user);
+    });
+  }).catch((e) => {
+    res.statusMessage = ERROR_STATUS_MESSAGE;
+    res.status(401).end();
+  });
+});
+
 module.exports = router;
