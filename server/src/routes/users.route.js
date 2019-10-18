62b5760c8cd3f0edff134c019badedfd28afbeec
Dmitry Shevchenko <myzenfeed@gmail.com>
2019-10-19 01:43:56 +0300

    add /users/token delete route handler for removing tokens from a user model

--- server/src/routes/users.route.js
+++ server/src/routes/users.route.js
@@ -38,4 +38,12 @@ router.get('/', auth, (req, res) => {
   res.send(req.user);
 });
 
+router.delete('/token', auth, (req, res) => {
+  req.user.removeToken(req.token).then(() => {
+    res.status(200).send();
+  }, () => {
+    res.status(400).send();
+  });
+});
+
 module.exports = router;
