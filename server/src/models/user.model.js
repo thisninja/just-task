3aba8c49cbc74379c43b837cdf4117e7d734336e
Dmitry Shevchenko <myzenfeed@gmail.com>
2019-10-19 00:48:58 +0300

    add email validation

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
+const EMAIL_REG_EXP = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
+
 const UserSchema = new mongoose.Schema({
   email: {
     type: String,
@@ -7,6 +9,12 @@ const UserSchema = new mongoose.Schema({
     trim: true,
     minlength: 1,
     unique: true,
+    validate: {
+      validator: function(email) {
+        return EMAIL_REG_EXP.test(email);
+      },
+      message: '{VALUE} is not a valid email'
+    }
   },
   password: {
     type: String,
