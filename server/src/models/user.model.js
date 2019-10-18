3a13837a7c69ebb7219e0d44610d09baf95c1ebc
Dmitry Shevchenko <myzenfeed@gmail.com>
2019-10-19 00:44:47 +0300

    add user schema

---
+++ server/src/models/user.model.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1,
+    unique: true,
+  },
+  password: {
+    type: String,
+    require: true,
+    minlength: 6
+  },
+  tokens: [{
+    access: {
+      type: String,
+      required: true
+    },
+    token: {
+      type: String,
+      required: true
+    }
+  }]
+});
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = { User }
