69e75494aeb565fe69da42dc0a07e6925497b7d3
Dmitry Shevchenko <myzenfeed@gmail.com>
2019-10-19 23:33:52 +0300

    add Signup component to router

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,4 +1,5 @@
 import Login from '../components/auth/Login.vue';
+import Signup from '../components/auth/Signup.vue';
 import Home from '../components/Home.vue';
 
 const routes = [
@@ -15,6 +16,7 @@ const routes = [
   {
     path: '/signup',
     name: 'SignUp',
+    component: Signup
   },
   {
     path: '*',
