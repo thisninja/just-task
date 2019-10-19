393b51bbc21354b491fd866af08a2aee50033016
Dmitry Shevchenko <myzenfeed@gmail.com>
2019-10-19 23:16:23 +0300

    update main.js

--- src/main.js
+++ src/main.js
@@ -2,18 +2,16 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 
 import App from './App';
-import routes from './routes';
+
+import router from './router/router';
+
+import store from './store/store';
 
 Vue.use(VueRouter);
 
-const router = new VueRouter({
-  routes,
-  linkActiveClass: 'active',
-  mode: 'history'
-});
-
 new Vue({
   el: '#app',
+  store,
   render: h => h(App),
   router
 });
