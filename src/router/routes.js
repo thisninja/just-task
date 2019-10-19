ff4407006dbde0c013b7a28f2fdb1cb86186678d
Dmitry Shevchenko <myzenfeed@gmail.com>
2019-10-19 23:29:50 +0300

    add Login and Home components to router

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,11 +1,16 @@
+import Login from '../components/auth/Login.vue';
+import Home from '../components/Home.vue';
+
 const routes = [
   {
     path: '/',
     name: 'Home',
+    component: Home,
   },
   {
     path: '/login',
     name: 'Login',
+    component: Login
   },
   {
     path: '/signup',
