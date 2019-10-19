23106406ab68e483b29c10a5d4e1e8d4914224c2
Dmitry Shevchenko <myzenfeed@gmail.com>
2019-10-19 23:27:00 +0300

    add Task placeholder

---
+++ src/components/tasks/Task.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="task-list">
+    Task list
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Task',
+};
+</script>
+
+<style lang="scss" scoped>
+.task-list {
+  min-width: 350px;
+  max-width: 700px;
+}
+</style>
