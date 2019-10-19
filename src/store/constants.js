03d5ff3935e38f4aeb26df40650898b7853e9bb7
Dmitry Shevchenko <myzenfeed@gmail.com>
2019-10-20 01:10:54 +0300

    add login and signup auth store actions

---
+++ src/store/constants.js
@@ -0,0 +1,3 @@
+export const EMAIL_IN_USE = 'The email is already in use!';
+export const LOGIN_ERROR_MSG_DEFAULT = 'An error happened during login!';
+export const SIGN_UP_ERROR_MSG_DEFAULT = 'An error happened during sign up!';
--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -1,4 +1,10 @@
 import axios from '../../services/Api';
+import router from '../../router/router';
+import {
+  EMAIL_IN_USE,
+  LOGIN_ERROR_MSG_DEFAULT,
+  SIGNUP_ERROR_MSG_DEFAULT,
+} from '../constants';
 
 const state = {
   idToken: null,
@@ -16,9 +22,84 @@ const getters = {
   },
 };
 
-const mutations = {};
+const mutations = {
+  authUser(state, payload) {
+    state.idToken = payload.token;
+    state.userId = payload.userId;
+    state.email = payload.email;
+  },
+  storeUser(state, payload) {
+    state.user = payload;
+  },
+  clearAuthData(state) {
+    state.idToken = null;
+    state.userId = null;
+  }
+};
 
-const actions = {};
+function authUser(res, { commit, dispatch }) {
+  commit('authUser', {
+    token: res.headers['x-access-token'],
+    userId: res.data._id,
+    email: res.data.email
+  });
+
+  const now = new Date();
+  const expiresIn = parseInt(res.headers['expires-in']);
+
+  const expirationDate = new Date(now.getTime() + expiresIn * 1000);
+
+  localStorage.setItem('token', res.headers['x-access-token']);
+  localStorage.setItem('userId', res.data._id);
+  localStorage.setItem('email', res.data.email);
+  localStorage.setItem('expirationDate', expirationDate);
+
+  dispatch('setLogoutTimer', expiresIn);
+}
+
+const actions = {
+  async signup({ commit, dispatch }, authData) {
+    try {
+      const res = await axios.post('/users', {
+        email: authData.email,
+        password: authData.password
+      });
+
+      authUser(res, { commit, dispatch });
+      router.push({ name: 'Home' });
+    } catch (error) {
+      let errorMsg = SIGNUP_ERROR_MSG_DEFAULT;
+      const response = error.response;
+
+      if (response && response.data && response.data.code === 11000) {
+        errorMsg = EMAIL_IN_USE;
+      }
+    }
+  },
+  async login({ commit, dispatch }, authData) {
+    try {
+      const res = await axios.post('/users/login', {
+        email: authData.email,
+        password: authData.password
+      });
+
+      authUser(res, { commit, dispatch });
+      router.push({ name: 'Home' });
+    } catch (error) {
+      let errorMsg = LOGIN_ERROR_MSG_DEFAULT;
+      const response = error.response;
+
+      if (response && typeof response.data === 'string') {
+        errorMsg = response.data;
+      }
+    }
+  },
+  setLogoutTimer({ commit }, expirationTime) {
+    setTimeout(() => {
+      commit('clearAuthData')
+    }, expirationTime * 1000)
+  },
+};
 
 export default {
   state,
