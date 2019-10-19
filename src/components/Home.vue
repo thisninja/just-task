ef75128c91936e7c119fb95b2d3acc0781af46b7
Dmitry Shevchenko <myzenfeed@gmail.com>
2019-10-19 23:28:45 +0300

    enable conditional component rendering for Home via render function

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,13 @@
 <script>
+import Login from './auth/Login.vue';
+import Task from './tasks/Task.vue';
+
 export default {
   name: 'Home',
   render(h) {
-    return h('div', { class: 'full-width' });
+    return h('div', { class: 'full-width' }, [
+      this.$store.getters.isAuthenticated ? h(Task) : h(Login)
+    ]);
   },
 }
 </script>
