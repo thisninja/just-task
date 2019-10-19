c4acaa412780b94930a22ac3e9be24379e64db24
Dmitry Shevchenko <myzenfeed@gmail.com>
2019-10-19 22:51:00 +0300

    add App placeholder

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,33 @@
 <template>
-    <div>
-      <Home />
+  <div class="app__wrapper">
+    <div class="app__container">
+        <router-view></router-view>
     </div>
+  </div>
 </template>
 
 <script>
-  import Home from './components/Home';
-  export default {
-    name: "App",
-    components: {
-      Home,
-    },
-  }
+export default {
+  name: 'App',
+}
 </script>
+
+<style lang="scss">
+.app {
+  &__wrapper {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  &__container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    max-width: 500px;
+    margin: 100px auto;
+    text-align: center;
+  }
+}
+</style>
