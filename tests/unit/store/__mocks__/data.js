ab9dde3cb02fedbb768cda0d806765cf3ec73295
Dmitry Shevchenko <myzenfeed@gmail.com>
2019-10-21 09:36:52 +0300

    add data mock

---
+++ tests/unit/store/__mocks__/data.js
@@ -0,0 +1,5 @@
+export const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZGE2YWI5YWRlMTIyMDExMDlmNjc3OWQiLCJhY2Nlc3MiOiJhdXRoIiwiaWF0IjoxNTcxNDE3MjE4fQ.zM-dPo_AtsZDvEobZpvobEAwQtauKvvHnYQEPetox4A';
+export const USER_ID = '5da6ab9ade12201109f6779d';
+export const EMAIL = 'test02@test.com';
+export const EMAIL_1 = 'test102@test.com';
+export const PASSWORD = '123asdQ!';
