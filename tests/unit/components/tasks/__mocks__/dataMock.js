e6ad1f196ce831b88b43057e6904fe67d5594717
Dmitry Shevchenko <myzenfeed@gmail.com>
2019-10-21 09:47:38 +0300

    add task mock

---
+++ tests/unit/components/tasks/__mocks__/dataMock.js
@@ -0,0 +1,17 @@
+const tasks = [{
+  _id: '5da6ab9ade12201109f6779f',
+  text: 'Do someting 1st time',
+  dueDate: 1572123600000,
+  _author: '5da6ab9ade12201109f6779d'
+}, {
+  _id: '5da6c1f2a1944115af1fe5c5',
+  text: 'Do someting 2nd time',
+  completed: true,
+  dueDate: 1572300000000,
+  completedAt: 1570395600000,
+  _author: '5da6ab9ade12201109f6779d'
+}];
+
+export default {
+  tasks,
+};
