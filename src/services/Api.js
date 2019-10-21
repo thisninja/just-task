f7b918574cd755cdd447cb30d2deccb55c1c8392
Dmitry Shevchenko <myzenfeed@gmail.com>
2019-10-21 09:24:07 +0300

    add BASE_URL variable for axios

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
+import {
+  BASE_URL
+ } from './constants';
+
 const instance = axios.create({
-  baseURL: 'http://localhost:8282/'
+  baseURL: BASE_URL,
 });
 
 export default instance;
